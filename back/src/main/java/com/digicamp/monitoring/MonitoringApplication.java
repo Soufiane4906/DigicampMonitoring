729b5302238fb909f108815a7ef753e4c1b3e5a9
Soufiane <126627087+Soufiane4906@users.noreply.github.com>
2025-10-12 01:16:25 +0100

    feat: add main application class

---
+++ back/src/main/java/com/digicamp/monitoring/MonitoringApplication.java
@@ -0,0 +1,14 @@
+package com.digicamp.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
+}
